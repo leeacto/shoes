f331c80e59ff99116d4c7d1cdca1fa182e72f235
Nicholas Lee <leeacto@Nicholass-MacBook-Pro-2.local>
2013-12-27 22:16:34 -0600

    starting OOJS

--- app/assets/javascripts/follow.js
+++ app/assets/javascripts/follow.js
@@ -1,12 +1,29 @@
-$.ready(function(){
-	$('.users').on('click', '.button_to', function(e){
+function userTable (el) {
+	this.el = $(el);
+	this.users = new Array;
+	this.loadUsers();
+}
+
+userTable.prototype.loadUsers = function (){
+  var	self = this;
+	$(this.el).children().each(function(){
+		if (this.id) {
+		  self.users.push(new Blurb(this.id))	
+		}
+	});
+}
+
+function Blurb (el) {
+	this.el = $('#' + el);
+	(this.el).find('.button_to').on('submit', function(e){
 		e.preventDefault();
 		var url = '';
-		var user_id = 
+		var user_id = '';
+		//This URL is wrong. write out full URL not path
 		if(this.text === 'Follow') {
-			url = 'user_follows_path'
+			url = 'user_follows_path';
 		} else {
-			url = 'user_follow_path'
+			url = 'user_follow_path';
 		}
 		$.ajax({
 			url: url,
@@ -14,4 +31,8 @@ $.ready(function(){
 			data: { id: user_id }
 		});
 	});
+}
+
+$(document).ready(function(){	
+	myTable = new userTable('.users');
 });
